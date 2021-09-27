e8f99afd09e064fdce4fc6d55fde794d4a5aa712
Christian Sorensen <78867945+Ruaffu@users.noreply.github.com>
2021-09-27 14:13:00 +0200

    constructor added

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -1,4 +1,24 @@
 package server;
 
-public class ClientHandler {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable{
+    Socket client;
+    PrintWriter pw;
+    Scanner scanner;
+
+    public ClientHandler(Socket socket) throws IOException {
+        this.client = socket;
+        this.pw = new PrintWriter(client.getOutputStream(),true);
+        this.scanner = new Scanner(client.getInputStream());
+    }
+
+    @Override
+    public void run() {
+
+        pw.println("Hello");
+    }
 }
